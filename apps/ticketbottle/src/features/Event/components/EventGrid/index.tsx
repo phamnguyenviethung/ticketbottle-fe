f92867ce2d2c942e26d8a6a993d2987d535725bb
Viet Hung <viethung01234@gmail.com>
2025-02-05 07:14:48 +0700

    refactor: improve configuration and enhance EventGrid component layout

--- apps/ticketbottle/src/features/Event/components/EventGrid/index.tsx
+++ apps/ticketbottle/src/features/Event/components/EventGrid/index.tsx
@@ -1,6 +1,17 @@
-import { Box, Image, SimpleGrid, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+  Tag,
+  Heading,
+  Flex,
+} from '@chakra-ui/react';
 import { Event, EventInfo } from '../../interfaces/event.interface';
 import { Link } from '@tanstack/react-router';
+import dayjs from 'dayjs';
 
 interface EventGridProps {
   data: Event[] | [];
@@ -16,8 +27,10 @@ const EventGrid: React.FC<EventGridProps> = ({ data }) => {
         lg: 4,
       }}
       w="full"
+      gap={4}
     >
       {data.map((event: Event) => {
+        const date = dayjs(event.eventInfo.startDate);
         const info: EventInfo = event.eventInfo;
         return (
           <Link
@@ -25,10 +38,36 @@ const EventGrid: React.FC<EventGridProps> = ({ data }) => {
             to="/event/$eventID"
             params={{ eventID: event.id }}
           >
-            <Box p={2}>
-              <Image src={'https://placehold.co/400x300'} />
-              <Text>{info.name}</Text>
-            </Box>
+            <Stack minH="350px" w="full" p={2}>
+              <Center w="full">
+                <Image
+                  src={event.eventInfo.thumbnail}
+                  alt={event.eventInfo.name}
+                />
+              </Center>
+              <Stack flex="1" mt={4}>
+                <Stack gap="2">
+                  <Box>
+                    <Tag.Root
+                      borderRadius="lg"
+                      variant="subtle"
+                      colorPalette="pink"
+                      size="lg"
+                    >
+                      <Tag.Label>Show sẽ huỷ</Tag.Label>
+                    </Tag.Root>
+                  </Box>
+                  <Text color="gray.600" fontWeight="500" fontSize="sm">
+                    {date.date()} tháng {date.month() + 1}, {date.year()}
+                  </Text>
+                </Stack>
+                <Flex alignItems="flex-end" flex="1">
+                  <Heading as="h1" fontWeight={600} fontSize="lg">
+                    {info.name}
+                  </Heading>
+                </Flex>
+              </Stack>
+            </Stack>
           </Link>
         );
       })}
--- apps/ticketbottle/vite.config.ts
+++ apps/ticketbottle/vite.config.ts
@@ -1,11 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { join } from 'path';
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
+  plugins: [
+    TanStackRouterVite({
+      routesDirectory: join(__dirname, 'src/routes'),
+      generatedRouteTree: join(__dirname, 'src/routeTree.gen.ts'),
+      quoteStyle: 'single',
+    }),
+    react(),
+    tsconfigPaths(),
+  ],
   server: {
     port: 5544,
+    fs: {
+      allow: [
+        searchForWorkspaceRoot(process.cwd()),
+        '...',
+        '..',
+        '../../node_modules',
+      ],
+    },
   },
 });
