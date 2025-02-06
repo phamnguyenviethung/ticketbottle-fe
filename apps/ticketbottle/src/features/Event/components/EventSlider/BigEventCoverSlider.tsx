a7e13b1e26d63014d3c11b2466e3e52a04e3df92
Viet Hung <viethung01234@gmail.com>
2025-02-06 13:21:46 +0700

    refactor: enhance BigEventCoverSlider with responsive settings and link integration

--- apps/ticketbottle/src/features/Event/components/EventSlider/BigEventCoverSlider.tsx
+++ apps/ticketbottle/src/features/Event/components/EventSlider/BigEventCoverSlider.tsx
@@ -1,9 +1,10 @@
-import { Box, Icon } from '@chakra-ui/react';
+import { Box, Icon, Image } from '@chakra-ui/react';
 import React from 'react';
 import Slider, { CustomArrowProps } from 'react-slick';
 import { Event } from '../../interfaces/event.interface';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { Skeleton } from '@/components/ui/skeleton';
+import { Link } from '@tanstack/react-router';
 
 interface BigEventCoverSliderProps {
   data: Event[] | [];
@@ -80,10 +81,29 @@ const settings = {
   dots: true,
   infinite: true,
   speed: 500,
-  slidesToShow: 1,
+  slidesToShow: 2,
   slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
   nextArrow: <SampleNextArrow />,
   prevArrow: <SamplePrevArrow />,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 const BigEventCoverSlider: React.FC<BigEventCoverSliderProps> = ({
   data,
@@ -100,17 +120,29 @@ const BigEventCoverSlider: React.FC<BigEventCoverSliderProps> = ({
           .filter((e: Event) => e.eventInfo)
           .map((event: Event) => {
             return (
-              <Box
-                borderRadius="lg"
-                key={event.id}
-                w="full"
-                h={{
-                  base: '200px',
-                  md: '400px',
+              <Link
+                to={`/event/$eventID`}
+                params={{
+                  eventID: event.id,
                 }}
-                backgroundImage={`url(${event.eventInfo.thumbnail})`}
-                backgroundSize="contain"
-              />
+                key={event.id}
+              >
+                <Box
+                  w="full"
+                  p={{
+                    base: 0,
+                    md: 4,
+                  }}
+                >
+                  <Image
+                    borderRadius="sm"
+                    w="full"
+                    maxH="400px"
+                    src={event.eventInfo.thumbnail}
+                    objectFit={'contain'}
+                  />
+                </Box>
+              </Link>
             );
           })}
       </Slider>
