42f2ff333cd974cd48ec71b7d6dd4fd7c905a8c3
Viet Hung <viethung01234@gmail.com>
2025-02-13 07:29:09 +0700

    feat: enhance order details display with structured layout and QR code generation

--- apps/ticketbottle/src/routes/_layout/_main-layout/(orders)/order.$code.tsx
+++ apps/ticketbottle/src/routes/_layout/_main-layout/(orders)/order.$code.tsx
@@ -1,14 +1,50 @@
 import orderAPI from '@/apis/order.api';
-import { Box } from '@chakra-ui/react';
+import PageLoader from '@/components/Loader/PageLoader';
+import { Box, Center, Heading, QrCode, Stack, Text } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
-import { DataList } from '@chakra-ui/react';
-
+import dayjs from 'dayjs';
 export const Route = createFileRoute(
   '/_layout/_main-layout/(orders)/order/$code'
 )({
   component: RouteComponent,
 });
+
+interface DataTitle {
+  label: string;
+  value: string | number;
+}
+
+const DataTitle: React.FC<DataTitle> = (props) => {
+  return (
+    <Box
+      direction="row"
+      display="flex"
+      justifyContent="space-between"
+      w="full"
+      my="2"
+    >
+      <Text
+        fontSize={{
+          base: 'sm',
+          md: 'md',
+        }}
+      >
+        {props.label}
+      </Text>
+      <Text
+        fontWeight={500}
+        textAlign="right"
+        fontSize={{
+          base: 'md',
+          md: 'lg',
+        }}
+      >
+        {props.value}
+      </Text>
+    </Box>
+  );
+};
 
 function RouteComponent() {
   const { code } = Route.useParams();
@@ -19,19 +55,102 @@ function RouteComponent() {
     select: (res) => res.data,
   });
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <PageLoader />;
+
+  const orderData: DataTitle[] = [
+    {
+      label: 'Họ và Tên',
+      value: data.firstName + ' ' + data.lastName,
+    },
+    {
+      label: 'Email',
+      value: data.email,
+    },
+    {
+      label: 'Mã đơn hàng',
+      value: data.code,
+    },
+    {
+      label: 'Mã giao dịch',
+      value: data.transactionId,
+    },
+    {
+      label: 'Ngày tạo',
+      value: dayjs(data.createdAt).format('DD/MM/YYYY HH:mm'),
+    },
+    {
+      label: 'Tổng tiền',
+      value: data.totalCheckOut,
+    },
+    {
+      label: 'Trạng Thái',
+      value: data.status,
+    },
+  ];
+
   return (
-    <Box>
-      <DataList.Root>
-        {Object.keys(data).map((k: string) => {
-          return (
-            <DataList.Item key={k}>
-              <DataList.ItemLabel>{k}</DataList.ItemLabel>
-              <DataList.ItemValue>{data[k]}</DataList.ItemValue>
-            </DataList.Item>
-          );
-        })}
-      </DataList.Root>
-    </Box>
+    <Stack w="full" gap={8}>
+      <Box w="full">
+        <Heading as="h6">Thông tin đơn hàng</Heading>
+        <Stack
+          w="full"
+          border="2px solid"
+          borderColor="green"
+          p="4"
+          borderRadius="md"
+        >
+          {orderData.map((i: DataTitle) => {
+            return <DataTitle {...i} key={i.label} />;
+          })}
+        </Stack>
+      </Box>
+      <Box w="full">
+        <Heading as="h6">Thông tin vé</Heading>
+        <Stack w="full">
+          {data.orderDetails.map(
+            (i: {
+              ticketclass: { name: string; price: string };
+              quantity: number;
+              tickets: { serialNumber: string }[];
+            }) => {
+              return (
+                <Stack
+                  key={i.ticketclass.name}
+                  w="full"
+                  border="2px dashed"
+                  p="4"
+                  borderRadius="lg"
+                >
+                  <DataTitle label="Tên vé" value={i.ticketclass.name} />
+                  <DataTitle label="Số lượng" value={i.quantity} />
+                  <DataTitle label="Giá vé" value={i.ticketclass.price} />
+                  <Box>
+                    <Stack gap={24}>
+                      {i.tickets.map((ticket: { serialNumber: string }) => {
+                        return (
+                          <Center
+                            flexDirection="column"
+                            gap={4}
+                            w="full"
+                            key={ticket.serialNumber}
+                          >
+                            <QrCode.Root value={ticket.serialNumber} size="xl">
+                              <QrCode.Frame>
+                                <QrCode.Pattern />
+                              </QrCode.Frame>
+                            </QrCode.Root>
+                            <Text fontWeight={600}>{ticket.serialNumber}</Text>
+                          </Center>
+                        );
+                      })}
+                    </Stack>
+                  </Box>
+                </Stack>
+              );
+            }
+          )}
+        </Stack>
+      </Box>
+    </Stack>
   );
 }
--- apps/ticketbottle/src/routes/_layout/_main-layout/_auth/(orders)/my-orders.tsx
+++ apps/ticketbottle/src/routes/_layout/_main-layout/_auth/(orders)/my-orders.tsx
@@ -1,6 +1,14 @@
 import orderAPI from '@/apis/order.api';
+import PageLoader from '@/components/Loader/PageLoader';
 import useAppStore from '@/store/useStore';
-import { Box, Text } from '@chakra-ui/react';
+import {
+  Badge,
+  Box,
+  Container,
+  Table,
+  TableScrollArea,
+  Text,
+} from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import { createFileRoute, Link as RouterLink } from '@tanstack/react-router';
 export const Route = createFileRoute(
@@ -18,18 +26,74 @@ function RouteComponent() {
     select: (res) => res.data,
   });
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <PageLoader />;
 
-  console.log(data);
   return (
-    <Box>
-      {data.data.map((i: { id: string }) => {
-        return (
-          <RouterLink to={`/order/$code`} params={{ code: i.id }} key={i.id}>
-            <Text _hover={{ color: 'blue.500' }}>Đơn hàng {i.id}</Text>
-          </RouterLink>
-        );
-      })}
-    </Box>
+    <Container maxW="container.lg">
+      <TableScrollArea borderWidth="1px">
+        <Table.Root
+          size="lg"
+          colorPalette="green"
+          variant="outline"
+          stickyHeader
+        >
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>Tên sự kiện</Table.ColumnHeader>
+              <Table.ColumnHeader textAlign="end">
+                Thanh toán
+              </Table.ColumnHeader>
+              <Table.ColumnHeader>Trạng thái</Table.ColumnHeader>
+              <Table.ColumnHeader></Table.ColumnHeader>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {data.data.map(
+              (i: {
+                id: string;
+                totalCheckOut: number;
+                status: string;
+                event: { eventInfo: { name: string } };
+              }) => {
+                return (
+                  <Table.Row key={i.id}>
+                    <Table.Cell>
+                      <Box
+                        textOverflow={'ellipsis'}
+                        overflow={'hidden'}
+                        whiteSpace={'nowrap'}
+                      >
+                        <Text fontWeight={600}>{i.event.eventInfo.name}</Text>
+                      </Box>
+                    </Table.Cell>
+                    <Table.Cell textAlign="end">
+                      <Text>{i.totalCheckOut}</Text>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Badge
+                        colorPalette={
+                          i.status === 'CANCELLED' ? 'red' : 'green'
+                        }
+                        variant="solid"
+                      >
+                        {i.status}
+                      </Badge>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <RouterLink to="/order/$code" params={{ code: i.id }}>
+                        <Text color="green.600" fontWeight={700}>
+                          Xem chi tiết
+                        </Text>
+                      </RouterLink>
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              }
+            )}
+          </Table.Body>
+        </Table.Root>
+      </TableScrollArea>
+    </Container>
   );
 }
